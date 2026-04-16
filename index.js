e591b075156ff341fd32c3bf3b387df50faacbea
Kushal Zinzuvadia <kushal.zinzuvadia@simformsolutions.com>
2026-04-16 15:42:33 +0530

    feat: Add logic for scrolling, add basic drag for scroll box

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+'use strict'
+
 const board = document.querySelector(".board");
-const scrollBox = document.querySelector(".scroll-box");
+const handle = document.querySelector(".scroll-handle");
+const scrollBar = document.querySelector(".scroll-tool");
+
+let isDragging = false;
+
+const startDrag = () => {
+    isDragging = true;
+    handle.style.cursor = "grabbing";
+};
+
+const duringDrag = (clientX) => {
+    if (isDragging) {
+        const scrollBarRect = scrollBar.getBoundingClientRect();
+        const handlePosition = clientX - scrollBarRect.left - handle.clientWidth / 2;
+        const maxPosition = scrollBar.clientWidth - handle.clientWidth;
+
+        const constrainedPosition = Math.max(0, Math.min(handlePosition, maxPosition));
+        handle.style.left = constrainedPosition + "px";
+
+        const scrollPercentage = constrainedPosition / maxPosition;
+        board.scrollLeft = scrollPercentage * (board.scrollWidth - board.clientWidth);
+    }
+};
+
+const stopDrag = () => {
+    isDragging = false;
+    handle.style.cursor = "grab";
+};
+
+board.addEventListener("scroll", () => {
+    const scrollPercentage = board.scrollLeft / (board.scrollWidth - board.clientWidth);
+    const handlePosition = scrollPercentage * (scrollBar.clientWidth - handle.clientWidth);
+    handle.style.left = handlePosition + "px";
+});
+
+handle.addEventListener("mousedown", startDrag);
+handle.addEventListener("mouseup", stopDrag);
+
+handle.addEventListener("mousemove", (e) => {
+    duringDrag(e.clientX);
+});
 
